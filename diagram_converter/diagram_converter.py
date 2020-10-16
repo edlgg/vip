f3d06efa1f691189bdef88fb4156ee81fbdfaeae
Eduardo De la Garza <edg@Eduardos-MacBook-Pro.local>
2020-10-16 18:17:00 -0500

    .

--- diagram_converter/diagram_converter.py
+++ diagram_converter/diagram_converter.py
@@ -129,8 +129,8 @@ def traverse_tree(node, pending):
 
 def get_rows():
     HOME = os.environ["HOME"]
-    #example_path = f"{HOME}/repos/vip/diagram_converter/examples/example1.csv"
-    example_path = "/Users/davidsouza/Documents/ITESM/11vo semestre/Compiladores/vip/diagram_converter/examples/example1.csv"
+    example_path = f"{HOME}/repos/vip/diagram_converter/examples/example1.csv"
+    # example_path = "/Users/davidsouza/Documents/ITESM/11vo semestre/Compiladores/vip/diagram_converter/examples/example1.csv"
     df = pd.read_csv(example_path, index_col=0)
 
     selected_page = 2
--- main.py
+++ main.py
@@ -2,36 +2,16 @@ from diagram_converter.diagram_converter import get_rows, get_tokens, print_rows
 from parser import parse
 
 # running 'python main.py' will only print something if there is a syntax error.
+
+
 def main():
 
-    #print_rows()
+    # print_rows()
 
     data = " "
     data = data.join(get_tokens())
 
-    # Temporary test data.
-    # TODO(Eduardo): Fix diagram_converter. We must add SEMICOLON at the end 
-    # of each 'if' and 'while'.
-    data = '''function main {
-            int a;
-            a = 1;
-            while(i < 5) {
-            if(i < 3) {
-            print("smaller than 3: ", i);
-            }
-            elif(i == 3) {
-            print("3: ", i);
-            }
-            else {
-            print("bigger than 3: ", i);
-            }
-            print(k);
-            }
-            }'''
-
     parse(data)
 
+
 main()
-
-
-
