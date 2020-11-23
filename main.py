6d0f69b69123ea2e971e3d470ccd411f530120d9
Eduardo De la Garza <eduardo@lambdacapital.xyz>
2020-11-22 19:44:44 -0600

    Added images

--- main.py
+++ main.py
@@ -7,23 +7,32 @@ import sys
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from constants import Type
 
 
 def main():
     type_ = sys.argv[1]
 
-    print_diagram(type_)
-
+    # Convert diagram to vip code.
     converted_diagram = convert_diagram(type_=type_)
 
     # Generate Object code.
     quadruples, constants = parse(converted_diagram)
 
     if len(sys.argv) > 2:
-        print(quadruples)
-        print(constants)
+        if sys.argv[2] == "1":
+            # Print png diagram and vip converted code.
+            print_diagram(type_)
+        if len(sys.argv) == 4:
+            if sys.argv[3] == "1":
+                # Print quadruples and address table content.
+                print("Quadruples:")
+                for i, q in enumerate(quadruples):
+                    print(i, q)
+                print("Constants:")
+                for key, value in constants.items():
+                    print(key, value)
 
-    print("")
     print("Virtual Machine output: ")
     vm = VirtualMachine(quadruples, constants)
     vm.run()
@@ -41,10 +50,10 @@ def print_diagram(type_):
 def convert_diagram(type_):
     dc = DiagramConverter()
     path = dc.get_example_path()
-    print("Printing the intermediate code")
     if len(sys.argv) > 3:
+        print("Printing the intermediate code")
         dc.print_rows(path, type_)
-    print("*******************************")
+        print("*******************************")
     dc = DiagramConverter()
     converted_diagram = dc.get_tokens(path, type_)
     converted_diagram = " ".join(converted_diagram)
